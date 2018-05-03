eddc07233c2bb8ba3d7bd646678a767fc2e68f1b
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-05-03 11:23:25 -0300

    Create new controllers
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
-  include ActionController::MimeResponds
-  respond_to :json
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  # include ActionController::MimeResponds
+  # respond_to :json
 end
