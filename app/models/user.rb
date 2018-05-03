2dd4f8418c929e25243271f7e5d79c36eb0e5865
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-05-03 11:23:51 -0300

    Configure new user model
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,22 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates_uniqueness_of :username
+  has_secure_token :auth_token
+
+  def invalidate_token
+    self.update_columns(auth_token: nil)
+  end
+
+  def self.validate_login(username, password)
+    user = find_by(username: username)
+    if user && user.authenticate(password)
+      user
+    end
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable,
+  #        :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 end
