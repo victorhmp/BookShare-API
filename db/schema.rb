5a0b4a78b6861401f0af8df7b31d1bb8eb5ef63b
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-05-03 11:22:04 -0300

    Create new User model
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_30_211921) do
+ActiveRecord::Schema.define(version: 2018_05_03_140253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,22 +55,13 @@ ActiveRecord::Schema.define(version: 2018_04_30_211921) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.string "first_name"
-    t.string "last_name"
+    t.string "username"
+    t.string "password_digest"
+    t.string "email"
+    t.string "name"
+    t.string "auth_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "list_items", "items"
