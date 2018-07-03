0d1a957df43adcf3622d56b3315643be89766e82
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-07-03 10:10:51 -0300

    Turn on static assets cache
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,12 @@ module Api
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins /\Ahttp:\/\/localhost:\d+\z/
-        resource '*', headers: :any, methods: :any
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins /\Ahttp:\/\/localhost:\d+\z/
+    #     resource '*', headers: :any, methods: :any
+    #   end
+    # end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
